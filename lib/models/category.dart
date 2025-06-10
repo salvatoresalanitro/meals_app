a1562cba65d93aecd1b1d803d9ee82729ad27a2c
Salvo <salvatoresalanitro94@gmail.com>
2025-06-10 17:05:27 +0200

    add folders to separate logically every item

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -4,9 +4,9 @@ class Category {
   const Category({
     required this.id,
     required this.title,
-    this.color = Colors.orange
+    this.color = Colors.orange //falbback, when is no provided this color will be used
   });
-  
+
   final String id;
   final String title;
   final Color color;
