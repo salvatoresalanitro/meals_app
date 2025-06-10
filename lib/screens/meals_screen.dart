6227f01095a33d85d173e793644f3ebe6e96fe76
Salvo <salvatoresalanitro94@gmail.com>
2025-06-10 18:39:08 +0200

    set layout for meals_screen

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -13,8 +13,47 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget content =
+        meals.isNotEmpty
+            ? ListView.builder(
+              itemCount: meals.length,
+              itemBuilder:
+                  (ctx, index) => Text(meals[index].title),
+            )
+            : Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Uh oh ...nothing here!',
+                    style: Theme.of(
+                      context,
+                    ).textTheme.headlineLarge!.copyWith(
+                      color:
+                          Theme.of(
+                            context,
+                          ).colorScheme.onSurface,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Try selecting a different category',
+                    style: Theme.of(
+                      context,
+                    ).textTheme.bodyLarge!.copyWith(
+                      color:
+                          Theme.of(
+                            context,
+                          ).colorScheme.onSurface,
+                    ),
+                  ),
+                ],
+              ),
+            );
+
     return Scaffold(
-      appBar: AppBar(title: Text('')),
+      appBar: AppBar(title: Text(title)),
+      body: content,
     );
   }
 }
