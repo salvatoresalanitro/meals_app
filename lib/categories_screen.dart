6d7f966b43fe559460b67ebe8300d7db95173885
Salvo <salvatoresalanitro94@gmail.com>
2025-06-10 16:58:38 +0200

    set grid with placeholder data

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -9,6 +9,23 @@ class CategoriesScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pick your category'),
       ),
+      body: GridView(
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 1.5,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+        children: [
+          Text('1'),
+          Text('2'),
+          Text('3'),
+          Text('4'),
+          Text('5'),
+          Text('6'),
+        ],
+      ),
     );
   }
 }
