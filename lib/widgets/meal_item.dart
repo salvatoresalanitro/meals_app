ef093bc4fb1f45e6681ee653d7042cb5693d24fa
Salvo <salvatoresalanitro94@gmail.com>
2025-06-11 09:48:20 +0200

    create meal_item.dart new widget

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
