fe389c9b0cbaaf924b70a99512ae2de073346dbd
Salvo <salvatoresalanitro94@gmail.com>
2025-06-13 10:19:07 +0200

    move logic from tabs screen to provider and refactor code

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/providers/meals_provider.dart';
 
 enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
 
@@ -11,7 +12,7 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
         Filter.vegan: false,
       });
 
-  void setAllFilters(Map<Filter,bool> chosenFilters){
+  void setAllFilters(Map<Filter, bool> chosenFilters) {
     state = chosenFilters;
   }
 
@@ -24,3 +25,23 @@ final filtersProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
       (ref) => FiltersNotifier(),
     );
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/providers/favorites_provider.dart';
 import 'package:meals_app/providers/filters_provider.dart';
-import 'package:meals_app/providers/meals_provider.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/filters_screen.dart';
 import 'package:meals_app/screens/meals_screen.dart';
@@ -44,27 +43,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider,); //ref is a variable available after extends to a ConsumerState
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals =
-        meals.where((meal) {
-          if (activeFilters[Filter.glutenFree]! &&
-              !meal.isGlutenFree) {
-            return false;
-          }
-          if (activeFilters[Filter.lactoseFree]! &&
-              !meal.isLactoseFree) {
-            return false;
-          }
-          if (activeFilters[Filter.vegetarian]! &&
-              !meal.isVegetarian) {
-            return false;
-          }
-          if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-            return false;
-          }
-          return true;
-        }).toList();
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
